5a6a2aa97e56e1b8087512298aaa9ff184012b35
nagyrajmund <rajmund977@gmail.com>
2021-05-25 17:36:10 +0200

    Fix a TF1 memory leak in dataset creation

--- src/scripts/create_dataset.py
+++ src/scripts/create_dataset.py
@@ -7,6 +7,7 @@ described in
 If the size of the combined segmentation maps is less than [10%] of the input size,
 the image is discarded.
 """
+import tensorflow as tf
 from scripts.integrated_gradients import update_progress_bar
 import shutil
 from src.models.wrappers.inpainting import InpaintingModel
@@ -29,7 +30,7 @@ segmentation_net_configs = [
 ]
 
 device                  = "cuda" if torch.cuda.is_available() else "cpu"
-n_images                = 100  # The number of desired images
+n_images                = 3000  # The number of desired images
 starting_idx            = 0    # The first image to start with
 confidence_threshold    = 0.3  # The minimum confidence for accepting an object segmentation
 image_input_dir         = "../../data/places365/validation_full"
@@ -42,19 +43,15 @@ def create_segmentation_dataset():
 
     if os.path.exists(segmentation_output_dir):
         cmd = input(
-            f"\nWARNING: output dir '{segmentation_output_dir}' already exists!" + \
-"""
-Type 'delete' to delete the folder,
-    'skip' to skip the segmentation, and
-    'keep' to continue saving to the folder.
-"""
+            f"\nWARNING: output dir '{segmentation_output_dir}' already exists!\n" + \
+            f"Press 'd' to delete and 's' to skip the segmentation, anything else will quit."
         )
     
-        if cmd == "delete":
+        if cmd.lower() == "d":
             shutil.rmtree(segmentation_output_dir)
-        elif cmd == "skip":
+        elif cmd.lower() == "s":
             return
-        elif cmd != "keep":
+        else:
             exit()
 
     os.makedirs(segmentation_output_dir)
@@ -103,37 +100,82 @@ Type 'delete' to delete the folder,
             break
 
 def create_inpainted_dataset():
-    if not os.path.exists(inpainting_output_dir):
-        os.makedirs(inpainting_output_dir)
-    else:
+    if os.path.exists(inpainting_output_dir):
         cmd = input(
-            f"\nWARNING: output dir '{inpainting_output_dir}' already exists!" + \
-"""
-Type 'delete' to delete the folder,
-    'skip' to skip the inpainting, and
-    'keep' to continue saving to the folder.
-"""
+            f"\nWARNING: output dir '{inpainting_output_dir}' already exists!\n" + \
+            f"Press 'd' to delete and 's' to skip the inpainting, anything else will quit."
         )
     
-        if cmd == "delete":
+        if cmd.lower() == "d":
             shutil.rmtree(inpainting_output_dir)
-        elif cmd == "skip":
+        elif cmd.lower() == "s":
             return
-        elif cmd != "keep":
+        else:
             exit()
+            
+    os.makedirs(inpainting_output_dir)
 
     # Only consider images that have segmentations
     filenames = os.listdir(segmentation_output_dir)
     progress_bar = tqdm(filenames, desc="Processing images")
     inpainting_model = InpaintingModel()
-    for filename in progress_bar:
-        input_image = cv2.imread(join(image_input_dir, filename))
-        segmentation = cv2.imread(join(segmentation_output_dir, filename))
+    
+    with inpainting_model.log_manager:
+        sess_config = tf.ConfigProto()
+        sess_config.gpu_options.allow_growth = True
+        temp_image = cv2.imread(join(image_input_dir, filenames[0]))    
+        h, w, _ = temp_image.shape
+        grid = 8
+        
+        # Preprocess the current image
+        transform = lambda img : np.expand_dims(img[:h//grid*grid, :w//grid*grid, :], 0)
+        with tf.Session(config=sess_config) as sess:
+            # Dirty, dirty tensorflow stuff to load the model 
+            sess.run(initialize_model(inpainting_model, transform(temp_image)))
+            
+            for filename in progress_bar:
+                # Open the input and it segmentation
+                original_image = cv2.imread(join(image_input_dir, filename))
+                segmentation = cv2.imread(join(segmentation_output_dir, filename))
+                
+                # Preprocess and concatenate them
+                image = transform(original_image)
+                mask = transform(segmentation)
+                input_image = np.concatenate([image, mask], axis=2)        
+                input_image = tf.constant(input_image, dtype=tf.float32)
+                
+                # Build the computational graph
+                output = inpainting_model.model.build_server_graph(
+                    inpainting_model.flags, input_image, reuse=tf.AUTO_REUSE)
+                output = (output + 1.) * 127.5
+                output = tf.reverse(output, [-1])
+                output = tf.saturate_cast(output, tf.uint8)
+                
+                # Run the computational graph
+                inpainted_image = sess.run(output)[0]
+
+                # Convert back to BGR and save with openCV
+                inpainted_image = cv2.cvtColor(inpainted_image, cv2.COLOR_RGB2BGR)
+                cv2.imwrite(join(inpainting_output_dir, filename), inpainted_image)
 
         inpainted_image = inpainting_model.inpaint(input_image, segmentation)
-        # Convert back to BGR and save
-        inpainted_image = cv2.cvtColor(inpainted_image, cv2.COLOR_RGB2BGR)
-        cv2.imwrite(join(inpainting_output_dir, filename), inpainted_image)
+
+
+def initialize_model(model, image):
+    "To be used as 'sess.run(initialize_model(...))'."
+    input_image = tf.constant(image, dtype=tf.float32)
+    output = model.model.build_server_graph(
+            model.flags, input_image, reuse=tf.AUTO_REUSE)
+    
+    vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
+    assign_ops = []
+    for var in vars_list:
+        vname = var.name
+        from_name = vname
+        var_value = tf.contrib.framework.load_variable("../../utils/deepfill_checkpoint/", from_name)
+        assign_ops.append(tf.assign(var, var_value))
+
+    return assign_ops
 
 if __name__ == "__main__":
     create_segmentation_dataset()
